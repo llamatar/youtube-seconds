a392502113311d0bf72cf63e3db717e1d7cc51f9
llamatar <joshuacheung777@gmail.com>
2022-08-12 23:49:11 -0700

    Inject content.js and change keys to u/p

--- content.js
+++ content.js
@@ -1,15 +1,11 @@
-const script = document.createElement('script');
-script.innerText = `
 window.addEventListener('keydown', function (e) {
   const player = document.querySelector('ytd-player').getPlayer();
   const seconds = 1;
-  if (e.keyCode === 85) { // 'u'
+  if (e.key === 'u') {
     player.seekToStreamTime(player.getCurrentTime() - seconds)
   }
-  else if (e.keyCode === 79) { // 'o'
+  else if (e.key === 'p') {
     player.seekToStreamTime(player.getCurrentTime() + seconds)
   }
 })
-`
-document.body.append(script);
 console.log('youtube-seconds extension loaded.');
