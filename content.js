05acc80f4932b479726ea633ba5f09845223b130
llamatar <joshuacheung777@gmail.com>
2022-08-12 22:57:08 -0700

    Set seek to 1s and keys u/o to backward/forward

--- content.js
+++ content.js
@@ -2,14 +2,12 @@ const script = document.createElement('script');
 script.innerText = `
 window.addEventListener('keydown', function (e) {
   const player = document.querySelector('ytd-player').getPlayer();
-  const seconds = 2;
-  if (e.ctrlKey) {
-    if (e.keyCode === 37) {
-      player.seekToStreamTime(player.getCurrentTime() - seconds)
-    }
-    else if (e.keyCode === 39) {
-      player.seekToStreamTime(player.getCurrentTime() + seconds)
-    }
+  const seconds = 1;
+  if (e.keyCode === 85) { // 'u'
+    player.seekToStreamTime(player.getCurrentTime() - seconds)
+  }
+  else if (e.keyCode === 79) { // 'o'
+    player.seekToStreamTime(player.getCurrentTime() + seconds)
   }
 })
 `
