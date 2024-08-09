6ae34e6fd37c40bfe2578376bbe9093f6b387749
llamatar <joshuacheung777@gmail.com>
2024-08-08 21:53:04 -0700

    Adjust indentation

--- content.js
+++ content.js
@@ -1,11 +1,21 @@
 window.addEventListener('keydown', function (e) {
-  const player = document.querySelector('ytd-player').getPlayer();
-  const seconds = 2;
-  if (e.key === 'u') {
-    player.seekToStreamTime(player.getCurrentTime() - seconds)
-  }
-  else if (e.key === 'p') {
-    player.seekToStreamTime(player.getCurrentTime() + seconds)
-  }
+	const player = document.querySelector('ytd-player').getPlayer();
+	switch (e.key) {
+		case 'u':
+			player.seekToStreamTime(player.getCurrentTime() - 2);
+			break;
+		case 'i':
+			player.seekToStreamTime(player.getCurrentTime() - 1);
+			break;
+		case 'o':
+			player.seekToStreamTime(player.getCurrentTime() + 1);
+			break;
+		case 'p':
+			player.seekToStreamTime(player.getCurrentTime() + 2);
+			break;
+		default:
+			return;
+	}
+	e.stopPropagation();
 })
 console.log('youtube-seconds extension loaded.');
